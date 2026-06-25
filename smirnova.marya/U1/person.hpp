e0ec1d6c03dff3d0250b421b6423e491b02833b6
SmirnovaMarya <unlenmaria@gmail.com>
2026-06-25 10:53:46 +0300

    add struct person

---
+++ smirnova.marya/U1/person.hpp
@@ -0,0 +1,16 @@
+#ifndef SMIRNOVA_PERSON_HPP
+#define SMIRNOVA_PERSON_HPP
+
+#include <string>
+
+namespace smirnova
+{
+  struct Person
+  {
+    size_t id;
+    std::string info;
+  };
+}
+
+#endif
+
